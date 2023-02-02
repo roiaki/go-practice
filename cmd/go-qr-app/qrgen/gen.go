eea3df8dd4cf8dd784f0da8bc28f4f6fa3e16b6b
roiaki <roitoitoitoi@yahoo.co.jp>
2023-02-02 16:41:25 +0900

    tab to spane

--- cmd/go-qr-app/qrgen/gen.go
+++ cmd/go-qr-app/qrgen/gen.go
@@ -1,23 +1,23 @@
 package qrgen
 
 import (
-	"bytes"
-	"image"
-	"image/png"
+    "bytes"
+    "image"
+    "image/png"
 
-	"github.com/boombuler/barcode"
-	"github.com/boombuler/barcode/qr"
+    "github.com/boombuler/barcode"
+    "github.com/boombuler/barcode/qr"
 )
 
 func CreateImage(b []byte) (image.Image, error) {
-	return png.Decode(bytes.NewReader(b))
+    return png.Decode(bytes.NewReader(b))
 }
 
 // GenQRCode generates QR code for the given URL
 func GenQRCode(url string, width, height int) (barcode.Barcode, error) {
-	qrCode, err := qr.Encode(url, qr.M, qr.Auto)
-	if err != nil {
-		return nil, err
-	}
-	return barcode.Scale(qrCode, width, height)
+    qrCode, err := qr.Encode(url, qr.M, qr.Auto)
+    if err != nil {
+        return nil, err
+    }
+    return barcode.Scale(qrCode, width, height)
 }
